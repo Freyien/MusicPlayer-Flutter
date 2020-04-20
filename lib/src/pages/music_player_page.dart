3898e6a832cb2df87219b79b99f8ea3209740360
Fernando Luis <ferb.stop@gmail.com>
2020-04-20 12:13:44 -0500

    Create workspace
    add assets

---
+++ lib/src/pages/music_player_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerPage extends StatelessWidget {
+  const MusicPlayerPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hola mundo'),
+      ),
+    );
+  }
+}
